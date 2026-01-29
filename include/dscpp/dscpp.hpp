7394c384449169d6bdf4e0b675052a27542109ea
Viktor Nikolov <viktor.nikolov069@gmail.com>
2026-01-29 21:57:05 +0200

    build: add umbrella headers for sequence and dscpp

---
+++ include/dscpp/dscpp.hpp
@@ -0,0 +1,12 @@
+#ifndef DSCPP_DSCPP_HPP
+#define DSCPP_DSCPP_HPP
+
+#include <dscpp/sequence.hpp>
+// Later:
+// #include <dscpp/adapters.hpp>
+// #include <dscpp/trees.hpp>
+// #include <dscpp/hashing.hpp>
+// #include <dscpp/graphs.hpp>
+// #include <dscpp/range_queries.hpp>
+
+#endif // DSCPP_DSCPP_HPP
---
+++ include/dscpp/sequence.hpp
@@ -0,0 +1,10 @@
+#ifndef DSCPP_SEQUENCE_HPP
+#define DSCPP_SEQUENCE_HPP
+
+#include <dscpp/sequence/static_array.hpp>
+// Later:
+// #include <dscpp/sequence/dynamic_array.hpp>
+// #include <dscpp/sequence/singly_linked_list.hpp>
+// #include <dscpp/sequence/doubly_linked_list.hpp>
+
+#endif // DSCPP_SEQUENCE_HPP
